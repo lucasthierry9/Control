97b77aa3ee92616da0b4bf9500bd5725251d9cac
lucasthierry9 <lucasthierry092@gmail.com>
2025-12-08 00:56:53 -0300

    adicionando a página de relatório de vendas, filtro de data e barra de pesquisa

--- vendas/views.py
+++ vendas/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render,get_object_or_404,redirect
 from control.models import Pedidos_Venda
 from . forms import PedidosVendaForm
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
+from django.db.models import Q
 
 
 #PEDIDOS
@@ -47,3 +49,44 @@ def excluir_pedido(request, id_pedido=0):
 def historico(request):
     pedidos = Pedidos_Venda.objects.filter(status__in=['concluido', 'cancelado'])
     return render(request, "vendas/historico/historico.html", {"pedidos": pedidos})
+
+#RELATÓRIO DE VENDAS
+@login_required
+def relatorio_vendas(request):
+    ano = request.GET.get("ano")
+    mes = request.GET.get("mes")
+    search = request.GET.get("search")
+
+    pedidos = Pedidos_Venda.objects.all()
+
+    if ano and ano.isdigit():
+        pedidos = pedidos.filter(data__year=ano)
+
+    if mes and mes.isdigit():
+        pedidos = pedidos.filter(data__month=mes)
+
+
+    if search:
+        pedidos = pedidos.filter(
+            Q(produto__nome__icontains=search) |
+            Q(cliente__nome__icontains=search) |
+            Q(vendedor__nome__icontains=search) 
+        )
+
+    pedidos = pedidos.order_by('-id')
+
+    # Lista de anos disponíveis
+    anos_disponiveis = Pedidos_Venda.objects.dates('data', 'year')
+
+    # Lista padrão de meses
+    meses_lista = [
+        ("1", "Janeiro"), ("2", "Fevereiro"), ("3", "Março"),
+        ("4", "Abril"), ("5", "Maio"), ("6", "Junho"),
+        ("7", "Julho"), ("8", "Agosto"), ("9", "Setembro"),
+        ("10", "Outubro"), ("11", "Novembro"), ("12", "Dezembro")
+    ]
+
+    paginator = Paginator(pedidos, 10)
+    numero_da_pagina = request.GET.get('p')
+    pedidos_paginados = paginator.get_page(numero_da_pagina)
+    return render(request, "vendas/relatorio/relatorio_vendas.html", {"pedidos": pedidos_paginados, "anos": [a.year for a in anos_disponiveis], "meses": meses_lista, "ano_selecionado": ano, "mes_selecionado": mes, "search": search,})
