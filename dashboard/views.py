e71edfde1d2cc2017af7d6fb6d0ebddce57e7551
cesar <fernandocesar4141@gmail.com>
2025-12-09 15:35:48 -0300

    add fixtures em json

--- dashboard/views.py
+++ dashboard/views.py
@@ -5,13 +5,18 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def index(request):
     return render(request, 'dashboard/index.html' )
+@login_required
 def charts(request):
     return render(request, 'dashboard/charts.html' )
+@login_required
 def tables(request):
     return render(request, 'dashboard/tables.html' )
+@login_required
 def vendas(request):
     return render(request, 'dashboard/vendas.html' )
+@login_required
 def clientes(request):
     return render(request, 'dashboard/clientes.html' )
+@login_required
 def produtos(request):
     return render(request, 'dashboard/produtos.html' )
